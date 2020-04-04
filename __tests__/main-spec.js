35244a07d61a01811f317e6341d127f91921e4c2
Songbai Yan <colorguitar@hotmail.com>
2020-04-04 20:40:20 +0800

    finish

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,5 +1,5 @@
 const createMultiplyTable = require('../main');
 
 it ('should create multiply table', () => {
-    expect(createMultiplyTable(2, 4)).toBe("123");
+    expect(createMultiplyTable(2, 4)).toBe("2*2=4 \n2*3=6 3*3=9 \n2*4=8 3*4=12 4*4=16 \n");
 });
--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-function createMultiplyTable (start, end) {
-    return start + end;
+function createMultiplyTable(start, end) {
+    if (checkStartNumberIsLessThanOrEqualsEndNumber(start, end)) {
+        table = ''
+        for (index = start; index <= end; index++) {
+            oneLine = generateOneLine(start, index)
+            table += oneLine + '\n'
+        }
+        return table
+    } else {
+        return 'null'
+    }
+}
+
+function generateOneLine(start, end) {
+    oneLine = ''
+    for (j = start; j <= end; j++){
+        oneExpression = generateOneExpression(j, end)
+        oneLine += oneExpression + ' '
+    }
+    return oneLine
+}
+
+function generateOneExpression(start, end) {
+    return start + '*' + end + '=' + start * end
+}
+
+function checkStartNumberIsLessThanOrEqualsEndNumber(start, end) {
+    return start <= end
 }
 
 module.exports = createMultiplyTable;
-
